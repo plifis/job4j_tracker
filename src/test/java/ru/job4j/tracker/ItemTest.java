985371ae00328495bb81f60502cb48525bbad5b1
plifis <nikich85@inbox.ru>
2021-09-16 21:43:53 +0300

    1. Интеграционные тесты с Hqldb [#4746 #184426]

--- src/test/java/ru/job4j/tracker/ItemTest.java
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -23,13 +23,10 @@ public class ItemTest {
     }
     @Test
     public void whenTestCompareToItemThenZToA() {
-        List<Item> list = Arrays.asList(
-                new Item("Nikita"),
-                new Item("Alexey"),
-                new Item("Tom")
-
-        );
-
+        List<Item> list = new ArrayList<>();
+        list.add(new Item("Nikita"));
+        list.add(new Item("Alexey"));
+        list.add(new Item("Tom"));
         list.sort(new SortItemFromZToA());
         List<Item> expected = new ArrayList<>();
         expected.add(new Item("Tom"));
